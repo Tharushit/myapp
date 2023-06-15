59e37a1f552f1a0231831d84c2fb34ac8cbf44d3
Tharushi Kuruwitage <thathsaraneekt.20@uom.lk>
2023-06-15 21:43:50 +0530

    Initial Commit
    Initial Commit

---
+++ src/AccessLevel/AccessLevel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AccessLevel = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default AccessLevel
--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+      <div className="card col-4 mt-2" style={{ backgroundColor: "#f1a8f8" }}>
+      <h5 class="card-header">
+        M.M. Abekoon <br/> 
+        abekoonmm@gmail.com <br/> 
+        Manager
+
+        <div className="profile-photo-container">
+          </div>
+      </h5>
+
+        <div class="card-body">
+
+          <h5 class="card-title">Give Super User permission</h5>
+
+          <p class="card-text">
+            <li>can sign in to the system</li>
+            <li>can log in to the system</li>
+            <li>can give user levels for other users</li>
+            <li>can confirm or cancel event</li>
+            <li>can participate events</li>
+            <li>can see information</li>
+            <li>can do payments through system</li>
+            <li>can see generated documents</li>
+            <li>should get notified when added to a event by system and email</li>
+            <li>can add photos to the shared album</li>
+            <li>can like and comment album</li>
+          </p>
+
+          <h5 class="card-title">Give Super User permission</h5>
+          <p class="card-text">
+            <li>can sign in to the system</li>
+            <li>can log in to the system</li>
+            <li>can give user levels for other users</li>
+            <li>can confirm or cancel event</li>
+            <li>can participate events</li>
+            <li>can see information</li>
+            <li>can do payments through system</li>
+            <li>can see generated documents</li>
+            <li>should get notified when added to a event by system and email</li>
+            <li>can add photos to the shared album</li>
+            <li>can like and comment album</li>
+          </p>
+
+          <h5 class="card-title">Give Super User permission</h5>
+          <p class="card-text">
+            <li>can sign in to the system</li>
+            <li>can log in to the system</li>
+            <li>can give user levels for other users</li>
+            <li>can confirm or cancel event</li>
+            <li>can participate events</li>
+            <li>can see information</li>
+            <li>can do payments through system</li>
+            <li>can see generated documents</li>
+            <li>should get notified when added to a event by system and email</li>
+            <li>can add photos to the shared album</li>
+            <li>can like and comment album</li>
+          </p>
+
+          <a href="#" class="btn btn-primary btn-lg btn-block btn-secondary">Grant User Access Levels</a>
+        </div>
+      </div>
     </div>
+
   );
 }
 
